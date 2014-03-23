df72e18649266d1c11dc64e0c019ade6ef836c33
Sergio Iván Burgueño <burguer@gmail.com>
2014-03-23 00:17:54 -0700

    clase 2014-1

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,21 @@
+class BlogController < ApplicationController
+
+def index
+	@fotos = Foto.all
+end
+
+def foto_nueva
+	@foto = Foto.new
+end
+
+def guardar_foto
+	@foto =  Foto.new(params[:foto])
+	if @foto.save
+		redirect_to :action => 'index'
+	else
+		#aki si no cumplio la validacion
+		render :action => 'foto_nueva'	
+	end
+end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fotoblog::Application.config.secret_token = '1af6f5830e5b007e4ea9aa610a28dfd3fb0fb7994a8ff996b1abd50e6aca1b9b9fd5f74ece13c34803d616539f648d08bc15a399761038616f95ee21022d3cc7'
